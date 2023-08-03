4b24efa889cbd4a29a5d3fa1b129069d8ea1494b
ClausR1990 <claus.reinhold@gmail.com>
2023-08-03 18:15:07 +0200

    added autocomplete addressfield

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,3 +1,80 @@
+'use client'
+
+import React, { useEffect, useState, Fragment } from 'react'
+import { Combobox, Transition } from '@headlessui/react'
+import { z } from 'zod'
+
+const addressData = z.object({
+  adgangsadresseid: z.string(),
+  id: z.string(),
+  status: z.number(),
+  darstatus: z.number(),
+  vejkode: z.string(),
+  vejnavn: z.string(),
+  adresseringsvejnavn: z.string(),
+  husnr: z.string(),
+  etage: z.string(),
+  dør: z.string(),
+  postnr: z.string(),
+  postnrnavn: z.string(),
+})
+
+const addressResponse = z.object({
+  data: z.array(addressData),
+  tekst: z.string(),
+  forslagstekst: z.string(),
+  caretpos: z.number(),
+})
+
+type AddressData = z.infer<typeof addressResponse>
+
 export const SearchBox: React.FC = () => {
-  return <></>
+  const [inputValue, setInputValue] = React.useState('')
+  const [selectedValue, setSelectedValue] = React.useState()
+  const [addressData, setAddressData] = useState<AddressData[] | null>(null)
+
+  console.log('🧑🏻', selectedValue)
+
+  useEffect(() => {
+    if (inputValue.length < 3) return
+    const getAddressData = async (value: string) => {
+      const response = await fetch(
+        `https://api.dataforsyningen.dk/autocomplete?q=${encodeURIComponent(
+          value,
+        )}&fuzzy`,
+      )
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data')
+      }
+      setAddressData(await response.json())
+    }
+    getAddressData(inputValue)
+  }, [inputValue])
+
+  return (
+    <Combobox value={selectedValue} onChange={setSelectedValue}>
+      <Combobox.Input
+        onChange={event => setInputValue(event.target.value)}
+        placeholder="Search address"
+        displayValue={(address: AddressData) => address.tekst}
+      />
+      <Transition
+        as={Fragment}
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        afterLeave={() => setInputValue('')}
+      >
+        <Combobox.Options>
+          {addressData &&
+            addressData.map((address, index) => (
+              <Combobox.Option key={index} value={address}>
+                {address.forslagstekst}
+              </Combobox.Option>
+            ))}
+        </Combobox.Options>
+      </Transition>
+    </Combobox>
+  )
 }
--- components/WeatherWidget/WeatherWidget.tsx
+++ components/WeatherWidget/WeatherWidget.tsx
@@ -2,6 +2,7 @@ import z from 'zod'
 import dayjs from 'dayjs'
 import 'dayjs/locale/da'
 import { degreesToCompass } from '@/utils/degreesToCompass'
+import { SearchBox } from '../SearchBox'
 
 const weather = z.object({
   description: z.string(),
@@ -46,38 +47,41 @@ export const WeatherWidget: React.FC<WeatherWidgetProps> = async ({ data }) => {
     ? `from-cyan-600 to-cyan-200`
     : `from-slate-900 to-indigo-900`
   return (
-    <div
-      className={`p-8 bg-gradient-to-r ${gradient} rounded-2xl shadow-2xl text-white text-2xl w-[800px] max-w-full relative capitalize overflow-hidden`}
-    >
-      <img
-        src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`}
-        alt=""
-        className="absolute right-0 top-0 h-full"
-      />
-      <h2 className="mb-4 text-xl">{data.weather[0].description}</h2>
-      <div className="flex flex-row divide-x items-center">
-        <div className="pr-4 text-8xl font-normal">
-          {Math.round(data.main.temp)}
-          <span className="font-thin">°</span>
-        </div>
-        <div className="pl-4">
-          <div className="text-xl">{currentDate}</div>
-          <div className="text-base font-bold">{data.name}</div>
-          <ul className="flex gap-2 text-sm mt-2">
-            <li className="">
-              Humidity: <b>{data?.main.humidity}</b>
-            </li>
-            <li className="">
-              Wind:{' '}
-              <b>
-                {Math.round(data?.wind.speed)}{' '}
-                <span className="lowercase">m/s</span>{' '}
-                {data?.wind.deg && degreesToCompass(data?.wind.deg)}
-              </b>
-            </li>
-          </ul>
+    <>
+      <div
+        className={`p-8 bg-gradient-to-r ${gradient} rounded-2xl shadow-2xl text-white text-2xl w-[800px] max-w-full relative capitalize overflow-hidden`}
+      >
+        <img
+          src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`}
+          alt=""
+          className="absolute right-0 top-0 h-full aspect-square"
+        />
+        <h2 className="mb-4 text-xl">{data.weather[0].description}</h2>
+        <div className="flex flex-row divide-x items-center">
+          <div className="pr-4 text-8xl font-normal">
+            {Math.round(data.main.temp)}
+            <span className="font-thin">°</span>
+          </div>
+          <div className="pl-4">
+            <div className="text-xl">{currentDate}</div>
+            <div className="text-base font-bold">{data.name}</div>
+            <ul className="flex gap-2 text-sm mt-2">
+              <li className="">
+                Humidity: <b>{data?.main.humidity}</b>
+              </li>
+              <li className="">
+                Wind:{' '}
+                <b>
+                  {Math.round(data?.wind.speed)}{' '}
+                  <span className="lowercase">m/s</span>{' '}
+                  {data?.wind.deg && degreesToCompass(data?.wind.deg)}
+                </b>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+      <SearchBox />
+    </>
   )
 }
